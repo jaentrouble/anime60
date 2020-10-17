afe38806e93194b67b1371f5ddad6a104799f867
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-18 02:18:16 +0900

    ffmpeg?

--- split_video_script.py
+++ split_video_script.py
@@ -3,15 +3,17 @@ import numpy as np
 import time
 from pathlib import Path
 import argparse
+import ffmpeg
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n','--name', dest='name')
 parser.add_argument('-e','--ext', dest='ext')
 args = parser.parse_args()
+
 raw_dir = Path('data/raw')
 cut_dir = Path('data/cut')
-name = 'ryza4k60_1'
-ext = '.mp4'
+name = args.name
+ext = args.ext
 
 start_time = time.time()
 n = 0
@@ -19,15 +21,29 @@ N = 1000
 total_frames = 0
 idx = 0
 done = False
-fourcc = cv2.VideoWriter_fourcc(*'X264')
+width, height = 3840, 2160
+# fourcc = cv2.VideoWriter_fourcc(*'X264')
+
+
+
 cap = cv2.VideoCapture(str(raw_dir/(name+ext)))
 while(cap.isOpened()):
-    writer = cv2.VideoWriter(str(cut_dir/f'{name}_{idx}{ext}'),fourcc, 60, (3840,2160)) 
+    # writer = cv2.VideoWriter(str(cut_dir/f'{name}_{idx}{ext}'),fourcc, 60, (3840,2160)) 
+    out_process = (
+        ffmpeg
+        .input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f'{width}x{height}')
+        .output(str(cut_dir/f'{name}_{idx}{ext}'), vcodec='h264_nvenc')
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+    )
     for i in range(N):
         if cap.isOpened():
             ret, frame = cap.read()
             if ret:
-                writer.write(frame)
+                out_process.stdin.write(
+                    frame.tobytes()
+                )
+                # writer.write(frame)
                 total_frames += 1
             else :
                 done = True
@@ -35,7 +51,8 @@ while(cap.isOpened()):
         else :
             done = True
             break
-    writer.release()
+    out_process.stdin.close()
+    out_process.wait()
     print(f'{idx} done')
     idx += 1
     if done:
