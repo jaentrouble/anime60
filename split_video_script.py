4a2e59166198e6831c096bc605479bb39a7b5443
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-18 03:43:05 +0900

    simple split

--- split_video_script.py
+++ split_video_script.py
@@ -8,60 +8,65 @@ import ffmpeg
 parser = argparse.ArgumentParser()
 parser.add_argument('-n','--name', dest='name')
 parser.add_argument('-e','--ext', dest='ext')
-parser.add_argument('-rc','--readcodec', dest='rcodec')
+# parser.add_argument('-rc','--readcodec', dest='rcodec')
+parser.add_argument('-d','--duration', dest='duration')
+parser.add_argument('-s','--split', dest='split')
 args = parser.parse_args()
 
 raw_dir = Path('data/raw')
 cut_dir = Path('data/cut')
 name = args.name
 ext = args.ext
-rcodec = args.rcodec
+duration = int(args.duration)
+split = int(args.split)
 
 start_time = time.time()
-n = 0
-N = 1000
-total_frames = 0
-idx = 0
-done = False
-width, height = 3840, 2160
+# width, height = 3840, 2160
 # fourcc = cv2.VideoWriter_fourcc(*'X264')
 
-in_process = (
-    ffmpeg
-    .input(str(raw_dir/(name+ext)),vcodec=rcodec)
-    .output('pipe:',format='rawvideo',pix_fmt='rgb24')
-    .run_async(pipe_stdout=True)
-)
-
-# cap = cv2.VideoCapture(str(raw_dir/(name+ext)))
-while True:
-    # writer = cv2.VideoWriter(str(cut_dir/f'{name}_{idx}{ext}'),fourcc, 60, (3840,2160)) 
-    out_process = (
+for i in range(split):
+    (
         ffmpeg
-        .input('pipe:', format='rawvideo', pix_fmt='rgb24', s=f'{width}x{height}')
-        .output(str(cut_dir/f'{name}_{idx}.mp4'), vcodec='h264_nvenc',
-        video_bitrate='500M')
-        .overwrite_output()
-        .run_async(pipe_stdin=True)
+        .input(str(raw_dir/(name+ext)),ss=duration*i, t=duration, c='copy')
+        .output(str(cut_dir/f'{name}_{i}.mp4')
+        .run()
     )
-    for i in range(N):
-        # ret, frame = cap.read()
-        in_bytes = in_process.stdout.read(width*height*3)
-        if in_bytes:
-            out_process.stdin.write(in_bytes)
-            # writer.write(frame)
-            total_frames += 1
-        else :
-            done = True
-            break
+# in_process = (
+#     ffmpeg
+#     .input(str(raw_dir/(name+ext)),vcodec=rcodec)
+#     .output('pipe:',format='rawvideo',pix_fmt='rgb24')
+#     .run_async(pipe_stdout=True)
+# )
 
-    out_process.stdin.close()
-    out_process.wait()
-    print(f'{idx} done')
-    idx += 1
-    if done:
-        break
-in_process.wait()
+# # cap = cv2.VideoCapture(str(raw_dir/(name+ext)))
+# while True:
+#     # writer = cv2.VideoWriter(str(cut_dir/f'{name}_{idx}{ext}'),fourcc, 60, (3840,2160)) 
+#     out_process = (
+#         ffmpeg
+#         .input('pipe:', format='rawvideo', pix_fmt='rgb24', s=f'{width}x{height}')
+#         .output(str(cut_dir/f'{name}_{idx}.mp4'), vcodec='h264_nvenc',
+#         video_bitrate='500M')
+#         .overwrite_output()
+#         .run_async(pipe_stdin=True)
+#     )
+#     for i in range(N):
+#         # ret, frame = cap.read()
+#         in_bytes = in_process.stdout.read(width*height*3)
+#         if in_bytes:
+#             out_process.stdin.write(in_bytes)
+#             # writer.write(frame)
+#             total_frames += 1
+#         else :
+#             done = True
+#             break
+
+#     out_process.stdin.close()
+#     out_process.wait()
+#     print(f'{idx} done')
+#     idx += 1
+#     if done:
+#         break
+# in_process.wait()
 
 # cap.release()
-print(f'{total_frames} frames took {time.time()-start_time} seconds ')
+print(f'took {time.time()-start_time} seconds ')
