0d9bbf6e47ef0977acd915fa867b637d510ab25e
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-18 11:17:27 +0900

    no audio?

--- split_video_script.py
+++ split_video_script.py
@@ -38,7 +38,7 @@ for v in os.listdir(raw_dir):
         try:
             (
                 ffmpeg
-                .input(str(raw_dir/v),ss=duration*i, t=duration)
+                .input(str(raw_dir/v),ss=duration*i, t=duration, an='')
                 .output(str(cut_dir/f'{Path(v).stem}_{i}.mp4'), 
                         vcodec='h264_nvenc',
                         video_bitrate='500M',)
