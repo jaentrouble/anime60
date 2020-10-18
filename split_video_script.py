9c018bc9c043d3236c1982665915db1a2cfa3f40
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-18 10:59:59 +0900

    no hide banner

--- split_video_script.py
+++ split_video_script.py
@@ -41,8 +41,7 @@ for v in os.listdir(raw_dir):
                 .input(str(raw_dir/v),ss=duration*i, t=duration)
                 .output(str(cut_dir/f'{Path(v).stem}_{i}.mp4'), 
                         vcodec='h264_nvenc',
-                        video_bitrate='500M',
-                        hide_banner=True,)
+                        video_bitrate='500M',)
                 .run()
             )
         except ffmpeg.Error as e:
