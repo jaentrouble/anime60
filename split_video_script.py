f3420f5126a135ea5a5c0d24673ee2982121c326
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-18 10:50:51 +0900

    autosplit

--- split_video_script.py
+++ split_video_script.py
@@ -5,37 +5,46 @@ from pathlib import Path
 import argparse
 import ffmpeg
 import sys
+import os
+import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-n','--name', dest='name')
-parser.add_argument('-e','--ext', dest='ext')
-# parser.add_argument('-rc','--readcodec', dest='rcodec')
 parser.add_argument('-d','--duration', dest='duration')
-parser.add_argument('-s','--split', dest='split')
 args = parser.parse_args()
 
 raw_dir = Path('data/raw')
 cut_dir = Path('data/cut')
-name = args.name
-ext = args.ext
 duration = int(args.duration)
-split = int(args.split)
 
 start_time = time.time()
-# width, height = 3840, 2160
-# fourcc = cv2.VideoWriter_fourcc(*'X264')
 
-for i in range(split):
-    try:
-        (
-            ffmpeg
-            .input(str(raw_dir/(name+ext)),ss=duration*i, t=duration)
-            .output(str(cut_dir/f'{name}_{i}.mp4'), vcodec='h264_nvenc',
-                    video_bitrate='500M')
-            .run()
-        )
-    except ffmpeg.Error as e:
-        print(e.stderr.decode(), file=sys.stderr)
-        sys.exit(1)
+for v in os.listdir(raw_dir):
+    vid_len = float(subprocess.run(
+        [
+            'ffprobe', 
+            '-v', 
+            'error', 
+            '-show_entries', 
+            'format=duration', 
+            '-of', 
+            'default=noprint_wrappers=1:nokey=1', 
+            str(raw_dir/v
+        ], 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.STDOUT).stdout
+    )
+    split = vid_len // duration
+    for i in range(split):
+        try:
+            (
+                ffmpeg
+                .input(str(raw_dir/v),ss=duration*i, t=duration)
+                .output(str(cut_dir/f'{name}_{i}.mp4'), vcodec='h264_nvenc',
+                        video_bitrate='500M')
+                .run()
+            )
+        except ffmpeg.Error as e:
+            print(e.stderr.decode(), file=sys.stderr)
+            sys.exit(1)
 
 print(f'took {time.time()-start_time} seconds')
