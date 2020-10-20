e9319a4c235b77e48f02ea99afa1a1ac984071f3
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-20 09:10:36 +0900

    message fix

--- model_trainer.py
+++ model_trainer.py
@@ -467,7 +467,7 @@ def run_training(
     delta = time.time()-st
     hours, remain = divmod(delta, 3600)
     minutes, seconds = divmod(remain, 60)
-    print(f'Took {hours}hours {minutes}minutes {seconds}seconds')
+    print(f'Took {hours:.0f} hours {minutes:.0f} minutes {seconds:.2f} seconds')
 
     test_ds = create_train_dataset(test_vid_paths,frame_size,batch_size,True)
     mymodel.evaluate(test_ds, steps=600)
