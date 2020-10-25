ecab036a3ceee9a9be1d22e02cfcf0f840e5872d
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-25 10:21:18 +0900

    backup

---
+++ to108060.py
@@ -0,0 +1,25 @@
+import os
+import ffmpeg
+from pathlib import Path
+inputdir = Path('D:/Lab/anime60/data/2160')
+outputdir = Path('D:/Lab/anime60/data/1080')
+kwargs={
+    'vcodec' : 'h264_nvenc',
+    'rc:v' : 'vbr_hq',
+    'cq:v' : '18',
+    'video_bitrate' : '30M',
+    'profile:v' : 'high',
+    'preset' : 'slow',
+    's' : '1920x1080'
+}
+for f in os.listdir(inputdir):
+
+
+    (
+        ffmpeg
+        .input(str(inputdir/f))
+        .video
+        .output(str(outputdir/f'{Path(f).stem}_1080.mp4'),
+                **kwargs)
+        .run()
+    )
