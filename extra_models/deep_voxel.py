3266dc2abb2b59086876353746fbb1b6243dc94e
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-10-25 20:18:11 +0900

    gamma 0.1 , 0.015

--- extra_models/deep_voxel.py
+++ extra_models/deep_voxel.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-GAMMA_FLOW = 0.03
+GAMMA_FLOW = 0.1
 GAMMA_MASK = 0.015
 
 class VoxelInterp(layers.Layer):
