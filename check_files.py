82d55674e0061da9c8b9ef880684280b211ebd2c
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-11-20 22:39:53 +0900

    wrong interp

--- check_files.py
+++ check_files.py
@@ -3,7 +3,8 @@ import cv2
 from pathlib import Path
 
 cut_dir = Path('data/cut')
-vids = os.listdir(cut_dir).sort()
+vids = os.listdir(cut_dir)
+vids.sort()
 for v in vids:
     print(f'checking {v}')
     cap = cv2.VideoCapture(str(cut_dir/v))
