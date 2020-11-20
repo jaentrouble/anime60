26da3c20003babd70f9f9172bb6f05dc830f4b9e
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-11-20 22:35:27 +0900

    check cut files

---
+++ check_files.py
@@ -0,0 +1,14 @@
+import os
+import cv2
+from pathlib import Path
+
+cut_dir = Path('data/cut')
+
+for v in os.listdir(cut_dir):
+    print(f'checking {v}')
+    cap = cv2.VideoCapture(str(cut_dir/v))
+    while cap.isOpened():
+        ret, _ = cap.read()
+        if not ret:
+            break
+    cap.release()
