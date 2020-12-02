6f7bd53bf988bd7bcfc0ff0d204132e69e23df8c
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-12-02 10:46:22 +0900

    static prefetch buffer

--- model_trainer_half.py
+++ model_trainer_half.py
@@ -336,7 +336,7 @@ def create_train_dataset(
     if not val_data:
         dataset = dataset.shuffle(2000, reshuffle_each_iteration=False)
     dataset = dataset.batch(batch_size, drop_remainder=True)
-    dataset = dataset.prefetch(autotune)
+    dataset = dataset.prefetch(100)
     dataset = dataset.repeat()
 
     return dataset
